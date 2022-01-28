8c1c8fcfd53d0826ec3d7167c762f86b3d2a3376
Deepak Sangle <deepaksangleok@gmail.com>
2022-01-28 15:14:24 +0530

    Frontend Completed
    Frontend of the website is almost done using HTML, CSS, JS.

---
+++ Pages.js
@@ -0,0 +1,69 @@
+let NavItems = ["home", "services", "prices", "about", "account"]; 
+
+DropList();
+
+function MobileNavigation() {
+    var x = document.getElementById("MobNav");
+    if (x.className === "MobileLI") {
+        document.getElementById("MobNav").className = 'AlternateMobileLI';
+        document.getElementById("Cross").style.display = "none";
+        document.getElementById("Dots").style.display = "block";
+    } else {
+        x.style.display = "block";
+        document.getElementById("MobNav").className = 'MobileLI';
+        document.getElementById("Dots").style.display = "none";
+        document.getElementById("Cross").style.display = "block";
+    }
+    
+    return false;
+}
+
+function DropList(){
+    for (let i=0, j=1;i<5;i++,j++) {
+        a = document.getElementById(NavItems[i]);
+        let x = "drop"+j;
+        xa = document.getElementById(x);
+        MouseOver(a,xa);
+    }
+}
+
+function MouseOver(a,xa){
+    a.addEventListener("mouseover", function(){
+        xa.style.opacity = "1";
+    });
+    a.addEventListener("mouseout", function(){
+        xa.addEventListener("mouseover", function(){
+            xa.style.opacity = "1";
+        } )
+        xa.style.opacity = "0";
+        xa.addEventListener("mouseout", function(){
+            xa.style.opacity = "0";
+        } )
+    });
+    console.log("FUC");
+}
+// let cls = document.getElementById("Box1");
+// let cls = document.getElementsByClassName("Box");
+// cls[1];
+// let cls = document.querySelector(".Box");
+// let cls = document.querySelectorAll(".Box~div");
+// console.log(cls);
+
+// let boxes = document.getElementsByClassName("Box");
+// console.log(typeof(boxes));
+// for(let i=0;i<boxes.length;i++){
+//     boxes[i].addEventListener("click", function(){
+//         boxes[i].style.backgroundSize = "110%";
+//     })
+// }
+
+// let x = confirm("Hey Sexy");
+// if(!x){
+//     let y = document.getElementById("BODY");
+//     y.style.display = "none";
+// }
+// console.log(`This is how "to add this"`);
+// function firstclick(){
+//     alert("BOZO");
+// }
+
