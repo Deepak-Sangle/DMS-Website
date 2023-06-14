6724302ca018cc891f7be86e93f8487ca5997de3
Deepak Sangle <sangleds20@iitk.ac.in>
2023-06-15 00:59:06 +0530

    initialize intellij idea

---
+++ inventory-server/src/main/java/com/dms/inventoryserver/InventoryServerApplication.java
@@ -0,0 +1,13 @@
+package com.dms.inventoryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServerApplication.class, args);
+	}
+
+}
